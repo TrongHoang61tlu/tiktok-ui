887bacba44776ca4917ae0de21eacc6bcaac2171
TrongHoang61tlu <tronghoang19112001@gmail.com>
2022-09-11 10:59:54 +0700

    Dung khung layout mac dinh

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,15 +1,19 @@
+import classNames from "classnames/bind";
+import styles from "./DefaultLayout.module.scss";
 import Header from "~/components/Layout/companents/Header";
 import SideBar from "./Sidebar";
-function DefaultLayout({children}) {
-    return ( 
-        <div>
-            <Header />
-            <div className="container">
-                <SideBar/>
-                <div className="content">{children }</div>
-            </div>
-        </div>
-     );
+
+const cx = classNames.bind(styles);
+function DefaultLayout({ children }) {
+  return (
+    <div className={cx("wrapper")}>
+      <Header />
+      <div className={cx("container")}>
+        <SideBar />
+        <div className={cx("content")}>{children}</div>
+      </div>
+    </div>
+  );
 }
 
 export default DefaultLayout;
